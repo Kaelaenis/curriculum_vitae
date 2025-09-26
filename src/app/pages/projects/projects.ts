a1d6199dd7ecfe89b4e835482f2e34f4051660d2
Kaelaenis <valentin.coulon5@outlook.fr>
2025-09-27 00:33:26 +0200

    Edit projects page with pseudo projects

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './projects.css'
 })
 export class Projects {
+  projects: Array<{title: string, description: string, imageUrl?: string, technologies: string[]}> = [];
 
+  constructor() {
+    this.loadProjects();
+  }
+
+  loadProjects() {
+    this.projects = [
+      {
+        title: 'Project 1',
+        description: 'Description for Project 1',
+        technologies: ['Angular', 'TypeScript', 'CSS'],
+        imageUrl: 'assets/images/profile.jpg'
+      },
+      {
+        title: 'Project 2',
+        description: 'Description for Project 2',
+        technologies: ['JavaScript', 'HTML', 'CSS'],
+      }
+    ];
+  }
 }
