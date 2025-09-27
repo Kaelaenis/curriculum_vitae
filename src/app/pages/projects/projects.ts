4f12e1da033c8b382fe64725c8d5c5934826b549
Kaelaenis <valentin.coulon5@outlook.fr>
2025-09-27 15:37:03 +0200

    Add 2 projects trough the TS file and adding a bit of style to it

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './projects.css'
 })
 export class Projects {
-  projects: Array<{title: string, description: string, imageUrl?: string, technologies: string[]}> = [];
+  projects: Array<{title: string, description: string, imageUrl?: string, technologies: string[], link?: string}> = [];
 
   constructor() {
     this.loadProjects();
@@ -16,16 +16,19 @@ export class Projects {
   loadProjects() {
     this.projects = [
       {
-        title: 'Project 1',
-        description: 'Description for Project 1',
-        technologies: ['Angular', 'TypeScript', 'CSS'],
-        imageUrl: 'assets/images/profile.jpg'
+        title: "Bot d'attribution rôles Discord",
+        description: 'Un bot Discord permettant d\'attribuer des rôles automatiquement en fonction des réactions. Version initiale en Node.js avec Discord.js qui demande soit une hébergement permanent, soit un démarrage manuel.',
+        technologies: ['Node.js', 'Discord.js'],
+        imageUrl: 'assets/images/discord-bot.jpg',
+        link: 'https://github.com/Kaelaenis/Discord_reactionrolebot'
       },
       {
-        title: 'Project 2',
-        description: 'Description for Project 2',
-        technologies: ['JavaScript', 'HTML', 'CSS'],
-      }
+        title: "Site de portfolio",
+        description: 'Un site web personnel pour présenter mes projets et compétences.',
+        technologies: ['JavaScript', 'HTML', 'CSS', 'Angular', 'Bootstrap', 'TypeScript', 'FontAwesome'],
+        imageUrl: 'assets/images/portfolio.jpg',
+        link: 'https://github.com/Kaelaenis/curriculum_vitae'
+      },
     ];
   }
 }
